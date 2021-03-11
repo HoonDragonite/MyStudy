ccd784ed8383c5e757f8fc44afe3bf47438d00bc
HoonDragonite <lsoonnoon@gmail.com>
2021-03-11 16:27:54 +0900

    파이썬 리스트

---
+++ Python/리스트(자바의 배열).py
@@ -0,0 +1,27 @@
+# 리스트란 무엇인가
+booksan = ["강백호", "정대만", "송태섭", "채치수", "서태웅"]
+neungnam = ["윤대협", "강태산", "변덕규", "허태환", "안영수"]
+
+print(booksan[0])
+print(booksan[-1])
+
+# 리스트 안의 리스트
+teams = ["해남", booksan, ["윤대협", "강태산", "기타등등"]]
+print(teams[0])
+print(teams[1][1])
+print(teams[2])
+
+# 리스트 슬라이싱
+# 문자열과 사용법은 동일하나 지정한 범위만큼을 리스트로 다시 리턴한다.
+print(booksan[:2])
+
+# 리스트 연산
+print(booksan + [" vs "] + neungnam)
+
+# 리스트의 길이
+print(len(booksan))
+
+# 리스트 요소 삭제
+tests = [1, 2, 3, 4, 5]
+del tests[3:] # 3 이후 삭제
+print(tests)
