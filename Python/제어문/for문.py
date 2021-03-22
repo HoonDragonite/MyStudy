fbd2cd42aef7f968829b2b32deaa7d72813fefee
HoonDragonite <lsoonnoon@gmail.com>
2021-03-22 10:37:15 +0900

    제어문

---
+++ Python/제어문/for문.py
@@ -0,0 +1,9 @@
+booksan = ['채치수', '서태웅', '강백호', '정대만', '송태섭']
+sum = 0
+
+for player in booksan:
+    print(player)
+
+for num in range(1, 11):
+    sum = sum + num
+print(sum)
---
+++ Python/제어문/while문.py
@@ -0,0 +1,10 @@
+count = 0
+num = 0
+
+while count < 3:
+    print(count)
+    count = count + 1
+    
+while num != 9:
+    print("9를 입력하세요")
+    num = int(input())
